9669b557d441fa5a77dcab895f36e4b24275601d
alleycaaat <achulslander@gmail.com>
2023-01-10 12:28:09 -0600

    updated favorites screen

--- screens/Favorites.js
+++ screens/Favorites.js
@@ -1,4 +1,5 @@
 import { useContext } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
 
 import MealsList from '../components/MealsList/MealsList';
 import { FavsContext } from '../store/context/favorites-context';
@@ -8,9 +9,29 @@ function Favorites() {
     const favMealsCtx = useContext(FavsContext);
     const favMeals = MEALS.filter(meal => favMealsCtx.ids.includes(meal.id));
 
+    if (favMeals.lenth === 0) {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.text}>No meals have been favorited.</Text>
+            </View>
+        );
+    }
     return (
         <MealsList items={favMeals} />
     );
 }
 
 export default Favorites;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    text: {
+        color: 'white',
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+});
